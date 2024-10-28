95aaf3bf824a6721d27e7eb1e700c60f5fa8c5b8
Luís Henrique Chesani <92533013+LChesani@users.noreply.github.com>
2024-10-28 05:00:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+rawData = None
+file_path = 'database/padaria_trab.json'
+
+
+if os.path.exists(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f: #o encoding ta tratando os caracteres especiais
+        rawData = json.load(f)
+else:
+    print(f"Error: The file '{file_path}' was not found.")
+
+
+#Data Preprocessing
+#na linha 737 tinha uma virgula faltando, foi adicionada manualmente no arquivo
+
+data = []
+for product in rawData: #como compra era uma coluna com valor nao significativo, foi removida, posso restaurar ela caso necessario setando um id
+    data.append(product['produtos']) #rawData é uma lista de dicionarios, transformamos em uma lista de listas de compras
+
+
+#Data Transformation
+
+#Transformando em formato tabular
+te = TransactionEncoder()
+te_ary = te.fit(data).transform(data)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+
+
+#Data Mining
+
+#apriori e regras
+frequent_itemsets = apriori(df, min_support=0.01, use_colnames=True)
+regras = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.7)
+
+
+print(regras)
